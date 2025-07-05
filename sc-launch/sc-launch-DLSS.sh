0fb1d4e602d083e28f9d06852613f990b25d3b41
Vogelhaufen <gh@killingonsight.de>
2025-07-06 00:12:20 +0200

    Update sc-launch-DLSS.sh

--- sc-launch/sc-launch-DLSS.sh
+++ sc-launch/sc-launch-DLSS.sh
@@ -169,6 +169,11 @@ export DXVK_LOG_LEVEL="error"
 export DXVK_NVAPIHACK="0"
 export DXVK_ENABLE_NVAPI="1"
 export PROTON_DXVK_D3D8="1"
+#disable all iGPU stuff
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __VK_LAYER_NV_optimus=NVIDIA_only
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
 
 # Optional HUDs (uncomment to enable)
 # export DXVK_HUD=fps
