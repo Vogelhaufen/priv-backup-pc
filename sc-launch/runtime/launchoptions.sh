11522b7ef88233c3c236d459f6315fccd4afe52c
Vogelhaufen <gh@killingonsight.de>
2025-07-09 23:57:27 +0200

    y

--- sc-launch/runtime/launchoptions.sh
+++ sc-launch/runtime/launchoptions.sh
@@ -314,47 +314,8 @@ case "$1" in
     ;;
 esac
 
-
-LIBCUDA_ORIG=""
-LIB_SEARCH_PATHS=(
-  /usr/lib
-  /usr/lib64
-  /usr/lib/x86_64-linux-gnu
-  /lib
-  /lib64
-  /lib/x86_64-linux-gnu
-  /usr/local/lib
-  /usr/local/lib64
-)
-
-for dir in "${LIB_SEARCH_PATHS[@]}"; do
-  for symlink in $(find "$dir" -type l -name "libcuda.so" 2>/dev/null); do
-    resolved=$(realpath "$symlink")
-    if [ -f "$resolved" ] && file "$resolved" | grep -q "ELF 64-bit"; then
-      LIBCUDA_ORIG="$resolved"
-      break 2
-    fi
-  done
-done
-
-if [ -z "$LIBCUDA_ORIG" ]; then
-  echo "✘ No 64-bit libcuda.so found!"
-  exit 1
-else
-  echo "✔ Found 64-bit libcuda.so at: $LIBCUDA_ORIG"
-fi
-
-export PATCHED_CUDA=$HOME/Games/star-citizen/libcuda.patched.so
-echo $LIBCUDA_ORIG
-echo $PATCHED_CUDA
-
-
-
 trap "update_check; \"$wine_path\"/wineserver -k" EXIT
 update_check() {
-  while mount | grep -q "$LIBCUDA_ORIG"; do sudo umount "$LIBCUDA_ORIG"; done
-  while mount | grep -q "$PATCHED_CUDA"; do sudo umount "$PATCHED_CUDA"; done
-
   "$wine_path"/wineserver -k
 }
 
