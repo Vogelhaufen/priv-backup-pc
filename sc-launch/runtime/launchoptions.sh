6d350dc3aa11241bfffde7590bba3d28edb8b00a
Vogelhaufen <gh@killingonsight.de>
2025-07-16 12:47:53 +0200

    Update launchoptions.sh

--- sc-launch/runtime/launchoptions.sh
+++ sc-launch/runtime/launchoptions.sh
@@ -179,6 +179,8 @@ extract_silent() {
 
 declare -A WINE_VERSIONS=(
 
+  ["10.12-DLSS-NTSYNC"]="https://github.com/starcitizen-lug/lug-wine/releases/download/10.12/lug-wine-tkg-staging-ntsync-git-10.12.tar.zst"
+  ["10.12-DLSS-FSYNC"]="https://github.com/starcitizen-lug/lug-wine/releases/download/10.12/lug-wine-tkg-staging-fsync-git-10.12.tar.zst"
   ["10.10-git"]="https://github.com/mactan-sc/mactan-sc-wine/releases/download/10.10-git/wine-tkg-staging-ntsync-git-10.10.tar.gz"
   ["10.8-git"]="https://github.com/mactan-sc/mactan-sc-wine/releases/download/10.8-git/wine-tkg-staging-ntsync-git-10.8.tar.gz"
   ["10.7-git"]="https://github.com/mactan-sc/mactan-sc-wine/releases/download/10.7-git/wine-tkg-staging-ntsync-git-10.7.r0.gedfe4935-327-x86_64.tar.gz"
@@ -190,7 +192,7 @@ declare -A WINE_VERSIONS=(
 BASE_DIR="$WINEPREFIX/runners"
 EXTRACT_DIR="$BASE_DIR/wine_runner"
 CONFIG_FILE="$BASE_DIR/.mactan_wine_version"
-DEFAULT_VERSION="10.10-git"
+DEFAULT_VERSION="10.12-DLSS-NTSYNC"
 
 mkdir -p "$BASE_DIR"
 
