07af414deec1a03855dc940e49f310b38d0f7549
Vogelhaufen <gh@killingonsight.de>
2025-07-16 11:11:08 +0200

    Update launchoptions.sh

--- sc-launch/runtime/launchoptions.sh
+++ sc-launch/runtime/launchoptions.sh
@@ -107,11 +107,10 @@ else
   printf "DXVK=%s\nNVAPI=%s\n" "$DXVK_VERSION" "$DXVK_NVAPI_VERSION" > "$VERSION_FILE"
 fi
 
-
 echo "============= Mactan Wine Runner Setup ==============="
 
 declare -A WINE_VERSIONS=(
-  ["10.12-git-DLSS"]="https://github.com/starcitizen-lug/lug-wine/releases/download/10.12/lug-wine-tkg-staging-ntsync-git-10.12.tar.zst"
+  ["10.12-git"]="https://github.com/starcitizen-lug/lug-wine/releases/download/10.12/lug-wine-tkg-staging-ntsync-git-10.12.tar.zst"
   ["10.10-git"]="https://github.com/mactan-sc/mactan-sc-wine/releases/download/10.10-git/wine-tkg-staging-ntsync-git-10.10.tar.gz"
   ["10.8-git"]="https://github.com/mactan-sc/mactan-sc-wine/releases/download/10.8-git/wine-tkg-staging-ntsync-git-10.8.tar.gz"
   ["10.7-git"]="https://github.com/mactan-sc/mactan-sc-wine/releases/download/10.7-git/wine-tkg-staging-ntsync-git-10.7.r0.gedfe4935-327-x86_64.tar.gz"
@@ -122,7 +121,7 @@ declare -A WINE_VERSIONS=(
 BASE_DIR="$PWD/runners"
 EXTRACT_DIR="$BASE_DIR/wine_runner"
 CONFIG_FILE="$BASE_DIR/.mactan_wine_version"
-DEFAULT_VERSION="10.12-git-DLSS"
+DEFAULT_VERSION="10.12-git"
 
 mkdir -p "$BASE_DIR"
 
@@ -163,7 +162,8 @@ else
 fi
 
 ARCHIVE_URL="${WINE_VERSIONS[$VERSION]}"
-ARCHIVE_PATH="$BASE_DIR/$VERSION.tar.gz"
+EXT="${ARCHIVE_URL##*.}"
+ARCHIVE_PATH="$BASE_DIR/$VERSION.tar.$EXT"
 
 if [ -d "$EXTRACT_DIR" ]; then
   echo "✔ Mactan runner already extracted, using existing files."
@@ -177,11 +177,19 @@ else
 
   echo "🗜 Extracting Mactan runner $VERSION to $EXTRACT_DIR (stripping top-level directory)..."
   mkdir -p "$EXTRACT_DIR"
-  tar xfz "$ARCHIVE_PATH" --strip-components=1 -C "$EXTRACT_DIR" || { echo "Extraction failed! Exiting."; exit 1; }
+  if [[ "$EXT" == "gz" ]]; then
+    tar xfz "$ARCHIVE_PATH" --strip-components=1 -C "$EXTRACT_DIR" || { echo "Extraction failed! Exiting."; exit 1; }
+  elif [[ "$EXT" == "zst" ]]; then
+    tar --use-compress-program=unzstd -xf "$ARCHIVE_PATH" --strip-components=1 -C "$EXTRACT_DIR" || { echo "Extraction failed! Exiting."; exit 1; }
+  else
+    echo "❌ Unsupported archive format: .$EXT"
+    exit 1
+  fi
 fi
 
 echo "Setup complete for Mactan Wine Runner version $VERSION."
 echo "To switch from $VERSION to another wine-runner: rm -rf rm $PWD/runners/.mactan_wine_version"
+
 
 # Setup fake DLLs for DLSS // only if not already existing
 
