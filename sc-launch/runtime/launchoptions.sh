009cd9e29fb4e20372c7f4e46ed60e23d8fb40b5
Vogelhaufen <gh@killingonsight.de>
2025-07-16 12:44:25 +0200

    Update launchoptions.sh

--- sc-launch/runtime/launchoptions.sh
+++ sc-launch/runtime/launchoptions.sh
@@ -415,8 +415,9 @@ trap update_check EXIT
 
 
 echo "=============== Launching Star Citizen ==============="
+export LAUNCH_LOG=sc_lauch.log
 # You can pin Cores to SC using taskset
 # e.g. /usr/bin/taskset -c 0-7,16-23 "$wine_path"/wine "C:\\Program Files\\Roberts Space Industries\\RSI Launcher\\RSI Launcher.exe" --disable-gpu --in-process-gpu > "$launch_log" 2>&1
 # Check your CPU specs
 # Not recommended as default
-"$wine_path"/wine "C:\\Program Files\\Roberts Space Industries\\RSI Launcher\\RSI Launcher.exe" --disable-gpu --in-process-gpu > "$launch_log" 2>&1
+"$wine_path"/wine "C:\\Program Files\\Roberts Space Industries\\RSI Launcher\\RSI Launcher.exe" --disable-gpu --in-process-gpu > "$LAUNCH_LOG" 2>&1
