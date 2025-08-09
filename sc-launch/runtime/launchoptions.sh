a8563f1c8215df68e4379dd242d83d5f9595e5ec
This is my GarbageHub. <gh@killingonsight.de>
2025-08-09 11:37:20 +0200

    Update launchoptions.sh

--- sc-launch/runtime/launchoptions.sh
+++ sc-launch/runtime/launchoptions.sh
@@ -37,8 +37,9 @@ export MESA_SHADER_CACHE_DIR="$WINEPREFIX"
 export MESA_SHADER_CACHE_MAX_SIZE="10G"
 
 # LAUNCH_LOG; 1=run without redirecting output (interactive CLI logging); 0=log to file
+# 0 is recommended to skate around bugs when logging to CLI
 export LAUNCH_LOG=sc-launch.log
-export CLI_LOG="1"
+export CLI_LOG="0"
 
 # LSFG settings
 # https://github.com/PancakeTAS/lsfg-vk
